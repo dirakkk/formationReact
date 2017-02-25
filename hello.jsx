caccc097f1ac3550809cad48f20d0e819daf1b2e
serra <dirakkk@hotmail.com>
2017-02-25 23:11:11 +0100

    etape4

--- hello.jsx
+++ hello.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 export class Hello extends Component {
 	render() {
-		return (<div>Avançons vers la lumière avec REACT</div>)
+		return (<div>Avançons vers la lumière avec {this.props.techno}</div>)
 	}
 }
--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {Hello} from './hello.jsx'
 ReactDOM.render(
-	<Hello/>,document.getElementById('container')
+	<Hello techno="REACT et ses props" />,document.getElementById('container')
 )
