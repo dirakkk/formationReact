3e974648cb9a1d97f4c36da60a9dec44f62920b5
dirakkk <yannick.serra.externe@darva.com>
2017-02-26 17:33:50 +0100

    etape5

--- hello.jsx
+++ hello.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 export class Hello extends Component {
+
+
+	constructor (props){
+		super(props)
+		this.state = {date: new Date()};
+	}
+
 	render() {
-		return (<div>Avançons vers la lumière avec {this.props.techno}</div>)
+		return (<div>Il est {this.state.date.toLocaleTimeString()},Nous avançons vers la lumière avec {this.props.techno}</div>)
 	}
 }
